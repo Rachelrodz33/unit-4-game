11aaba0aec05a1992a87f364d13c7efd0b75eac1
Rachel Rodriguez <rachelrodriguez@cpe-172-100-3-75.twcny.res.rr.com>
2019-01-12 16:57:07 -0600

    second commit w pics

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//variables I guess
+
+//to start game should show a random number 
+
+//gems assigned a specific number
+
+//when clicked the gem adds to the player's score
+
+//game ends if the score is the same as the random number
+
+//player loses if the score is higher that the random number 
+
+//show wins and losses
+
+//game resets when win/lose 
